b6121786dfe0a7ca18f9411cadd486aadf910bdc
Joey DeFrancesco <jdefr89@gmail.com>
2011-06-01 01:10:10 -0400

    making progress... next: code define_word

--- src/turntext.py
+++ src/turntext.py
@@ -1,10 +1,17 @@
 # TurnText - A swiss army knife for working with words 
 # 
-# Distributed under LGPL (http://www.gnu.org/copyleft/lesser.html)
-# Written by : Joey DeFrancesco
-# Modified: May 11, 2011 @ 2:21PM
+# Description: TurnText aims to be a simple, light-weight command line dictionary/word
+#              utility. TurnText will clean and efficient, delivering high quality
+#              definitions and examples for any word you would like the meaning of.
+#               
+#              TurnText relies heavily on (and may in some sense be a wrapper) the Worknik API.
+#              The use of WordNik allows for quality definitions and examples as well as other
+#              features you may find useful.
 #
-# Note: TurnText relies heavily on wordnik's api and python library.
+# Distributed: LGPL (http://www.gnu.org/copyleft/lesser.html)
+# Written by : Joey DeFrancesco
+# Modified: May 24, 2011 @ 11:52 PM
+#
 # Wordnik API Key: d5a58307aef66a63651080454b601a8f235f7445be04adcba
 
 #!/usr/bin/env python
@@ -35,7 +42,7 @@ DEFAULT_EXAMPLE_COUNT = 1
 
 def DEBUG(): print '***DEBUG***'
 
-def showHelp(): 
+def show_help(): 
 
     
     # parse arguments, optlist contains flags and corresponding options.
@@ -49,15 +56,23 @@ def showHelp():
     # TEMPORARY FOR TESTING
     print """
     python turntext.py [ARGS]
-        w - word of day
-        r - random list of words
+        w - display word of day
+        r - output a single random word (fun to learn new words)
+        l - output a random list of words
         d - number of definitions to display
             for a single word. Defaults to 2
             of the top rated definitions.
     """
 
-# word_of_day() - Display word of the day
-# ARGS: Wordnik object
+# Function: define_word(targetWord, definitionCount, exampleCount)
+# In: target word, number of definitions to return, 
+# Out: 
+# Description: Fetches word defintion along with example uses using the Wordnik API
+define_word(): pass
+
+# word_of_day() - Display word of the day and its definition
+# Arguments: Wordnik object
+# NOTE: Function exits program.
 def word_of_day(word):
 
     # Wordnik API returns JSON data
@@ -70,23 +85,24 @@ def word_of_day(word):
 
     for key, val in words.iteritems():
         print key, '=>', val
-    # we exit, as we only want word of the day.
-    sys.exit(0)
 
+# return list of random words
 def get_random_list(): pass
+
+# return one random word
 def get_random_word(): pass
 
 def main():
 
     if len(sys.argv) < 2:
-        showHelp()
+        show_help()
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'wrld:e: ')
     except getopt.GetoptError, err:
         # help information, then exit
         print str(err)
-        showHelp()
+        show_help()
         sys.exit(1)
 
     # Create Wordnik object, Authenticate with API key (See header comment block)
@@ -96,16 +112,17 @@ def main():
     # Number of definitions and examples to show (Default)
     definitionCount = DEFAULT_DEFINITION_COUNT
     exampleCount = DEFAULT_EXAMPLE_COUNT
-    
+
     for o, a in opts:
         if o == '-w': # display word of the day, then exit.
-            word_of_day(word)
-            # single option - exit
+            get_word_of_day(word)
+
         elif o == '-r': # get random word
-            
-            # single option - exit
+            get_random_word()
+
         elif o == '-l': # get random list of words
-           get_random_list()  
+            get_random_list()  
+
         elif o == '-d':
             # MAKE SURE THEY SUPPLY NUMBER (RESTRICT 10)
             # Specify how many definitions to display
@@ -118,6 +135,14 @@ def main():
                 exampleCount = int(a)
         else:
             print "Invalid flag <== show help"
+
+    if len(args) == 0:
+        return 0 # no word was supplied to be defined
+
+    # target word to define is our first (and only for now) element in args list
+    targetWord = args[0]
+
+    define_word(targetWord, definitionCount, exampleCount)
 
 # Handles command line arguments, then call main()
 if __name__ == '__main__':
